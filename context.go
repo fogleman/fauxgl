2de9b55cd27978a6e64a8965e0053ae7fd21360f
Michael Fogleman <fogleman@gmail.com>
2017-01-31 15:53:29 -0500

    forward differencing rasterization

--- context.go
+++ context.go
@@ -45,43 +45,53 @@ func (dc *Context) ClearDepthBuffer() {
 	}
 }
 
-func (dc *Context) rasterize(v1, v2, v3 Vertex, s1, s2, s3 Vector, shader Shader) {
-	min := s1.Min(s2.Min(s3)).Floor()
-	max := s1.Max(s2.Max(s3)).Ceil()
-	x1 := int(min.X)
-	x2 := int(max.X)
-	y1 := int(min.Y)
-	y2 := int(max.Y)
-	ra := 1 / ((s3.X-s1.X)*(s2.Y-s1.Y) - (s3.Y-s1.Y)*(s2.X-s1.X))
+func edge(a, b, c Vector) float64 {
+	return (b.X-a.X)*(c.Y-a.Y) - (b.Y-a.Y)*(c.X-a.X)
+}
+
+func (dc *Context) rasterize(v0, v1, v2 Vertex, s0, s1, s2 Vector, shader Shader) {
+	min := s0.Min(s1.Min(s2)).Floor()
+	max := s0.Max(s1.Max(s2)).Ceil()
+	x0 := int(min.X)
+	x1 := int(max.X)
+	y0 := int(min.Y)
+	y1 := int(max.Y)
+	p := Vector{float64(x0) + 0.5, float64(y0) + 0.5, 0}
+	w00 := edge(s1, s2, p)
+	w01 := edge(s2, s0, p)
+	w02 := edge(s0, s1, p)
+	a01 := s0.Y - s1.Y
+	b01 := s1.X - s0.X
+	a12 := s1.Y - s2.Y
+	b12 := s2.X - s1.X
+	a20 := s2.Y - s0.Y
+	b20 := s0.X - s2.X
+	ra := 1 / edge(s0, s1, s2)
+	r0 := 1 / v0.Output.W
 	r1 := 1 / v1.Output.W
 	r2 := 1 / v2.Output.W
-	r3 := 1 / v3.Output.W
-	for y := y1; y <= y2; y++ {
-		for x := x1; x <= x2; x++ {
-			p := Vector{float64(x) + 0.5, float64(y) + 0.5, 0}
-			bx := (p.X-s2.X)*(s3.Y-s2.Y) - (p.Y-s2.Y)*(s3.X-s2.X)
-			if bx < 0 {
+	for y := y0; y <= y1; y++ {
+		w0 := w00
+		w1 := w01
+		w2 := w02
+		for x := x0; x <= x1; x++ {
+			b0 := w0 * ra
+			b1 := w1 * ra
+			b2 := w2 * ra
+			w0 += a12
+			w1 += a20
+			w2 += a01
+			if b0 < 0 || b1 < 0 || b2 < 0 {
 				continue
 			}
-			by := (p.X-s3.X)*(s1.Y-s3.Y) - (p.Y-s3.Y)*(s1.X-s3.X)
-			if by < 0 {
-				continue
-			}
-			bz := (p.X-s1.X)*(s2.Y-s1.Y) - (p.Y-s1.Y)*(s2.X-s1.X)
-			if bz < 0 {
-				continue
-			}
-			bx *= ra
-			by *= ra
-			bz *= ra
-			z := bx*s1.Z + by*s2.Z + bz*s3.Z
+			z := b0*s0.Z + b1*s1.Z + b2*s2.Z
 			i := y*dc.Width + x
 			if z >= dc.DepthBuffer[i] { // completely safe?
 				continue
 			}
-			b := VectorW{bx * r1, by * r2, bz * r3, 0}
+			b := VectorW{b0 * r0, b1 * r1, b2 * r2, 0}
 			b.W = 1 / (b.X + b.Y + b.Z)
-			v := InterpolateVertexes(v1, v2, v3, b)
+			v := InterpolateVertexes(v0, v1, v2, b)
 			color := shader.Fragment(v)
 			if color == Discard {
 				continue
@@ -95,27 +105,26 @@ func (dc *Context) rasterize(v1, v2, v3 Vertex, s1, s2, s3 Vector, shader Shader
 			}
 			lock.Unlock()
 		}
+		w00 += b12
+		w01 += b20
+		w02 += b01
 	}
 }
 
-func (dc *Context) drawClipped(v1, v2, v3 Vertex, shader Shader) {
+func (dc *Context) drawClipped(v0, v1, v2 Vertex, shader Shader) {
+	ndc0 := v0.Output.DivScalar(v0.Output.W).Vector()
 	ndc1 := v1.Output.DivScalar(v1.Output.W).Vector()
 	ndc2 := v2.Output.DivScalar(v2.Output.W).Vector()
-	ndc3 := v3.Output.DivScalar(v3.Output.W).Vector()
 
 	// back face culling
-	var sum float64
-	sum += (ndc2.X - ndc1.X) * (ndc2.Y + ndc1.Y)
-	sum += (ndc3.X - ndc2.X) * (ndc3.Y + ndc2.Y)
-	sum += (ndc1.X - ndc3.X) * (ndc1.Y + ndc3.Y)
-	if sum >= 0 {
+	if (ndc1.X-ndc0.X)*(ndc2.Y-ndc0.Y)-(ndc2.X-ndc0.X)*(ndc1.Y-ndc0.Y) <= 0 {
 		return
 	}
 
+	s0 := dc.screenMatrix.MulPosition(ndc0)
 	s1 := dc.screenMatrix.MulPosition(ndc1)
 	s2 := dc.screenMatrix.MulPosition(ndc2)
-	s3 := dc.screenMatrix.MulPosition(ndc3)
-	dc.rasterize(v1, v2, v3, s1, s2, s3, shader)
+	dc.rasterize(v0, v1, v2, s0, s1, s2, shader)
 }
 
 func (dc *Context) DrawTriangle(t *Triangle, shader Shader) {
