8c9323b6ed4ef22161a73fec7e040d9b68aaede0
Michael Fogleman <fogleman@gmail.com>
2017-02-03 22:29:43 -0500

    update example

--- examples/magica.go
+++ examples/magica.go
@@ -25,18 +25,46 @@ var (
 	light  = V(-0.25, -0.75, 1).Normalize() // light direction
 )
 
+func axisAligned(p1, p2 Vector) bool {
+	count := 0
+	if p1.X == p2.X {
+		count++
+	}
+	if p1.Y == p2.Y {
+		count++
+	}
+	if p1.Z == p2.Z {
+		count++
+	}
+	return count >= 2
+}
+
 func main() {
 	// load a mesh
 	mesh, err := LoadVOX(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println(len(mesh.Triangles))
-	// mesh.SaveSTL("out.stl")
 
 	// fit mesh in a bi-unit cube centered at the origin
 	mesh.BiUnitCube()
+
+	var lines []*Line
+	for _, t := range mesh.Triangles {
+		p1 := t.V1.Position
+		p2 := t.V2.Position
+		p3 := t.V3.Position
+		if axisAligned(p1, p2) {
+			lines = append(lines, NewLine(t.V1, t.V2))
+		}
+		if axisAligned(p2, p3) {
+			lines = append(lines, NewLine(t.V2, t.V3))
+		}
+		if axisAligned(p3, p1) {
+			lines = append(lines, NewLine(t.V3, t.V1))
+		}
+	}
 
 	// create a rendering context
 	context := NewContext(width*scale, height*scale)
@@ -47,7 +75,7 @@ func main() {
 	matrix := LookAt(eye, center, up).Orthographic(-1.45, 1.45, -1.45, 1.45, -20, 20)
 
 	// render
-	context.ClearColorBufferWith(Black)
+	context.ClearColorBufferWith(HexColor("323"))
 	ambient := Color{0.4, 0.4, 0.4, 1}
 	diffuse := Color{0.9, 0.9, 0.9, 1}
 	context.Shader = NewDiffuseShader(matrix, light, Discard, ambient, diffuse)
@@ -55,11 +83,12 @@ func main() {
 	context.DrawMesh(mesh)
 	fmt.Println(time.Since(start))
 
-	context.Shader = NewSolidColorShader(matrix, Black)
+	context.Shader = NewSolidColorShader(matrix, HexColor("000"))
 	context.Wireframe = true
-	context.DepthBias = -1e-5
+	context.LineWidth = 8
+	context.DepthBias = -1e-4
 	start = time.Now()
-	context.DrawMesh(mesh)
+	context.DrawLines(lines)
 	fmt.Println(time.Since(start))
 
 	// downsample image for antialiasing
