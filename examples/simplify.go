00fae98770db0c3e3f1847b8aeccc1685c7b5df6
Michael Fogleman <fogleman@gmail.com>
2017-02-16 21:37:40 -0500

    wip

--- examples/simplify.go
+++ examples/simplify.go
@@ -10,19 +10,19 @@ import (
 )
 
 const (
-	scale  = 4    // optional supersampling
-	width  = 1600 // output width in pixels
-	height = 1600 // output height in pixels
-	fovy   = 25   // vertical field of view in degrees
+	scale  = 2    // optional supersampling
+	width  = 2048 // output width in pixels
+	height = 2048 // output height in pixels
+	fovy   = 10   // vertical field of view in degrees
 	near   = 1    // near clipping plane
 	far    = 100  // far clipping plane
 )
 
 var (
 	eye        = V(3, 3, 3)                 // camera position
-	center     = V(0, 0, -0.1)              // view center position
+	center     = V(0.15, 0, 0.3)            // view center position
 	up         = V(0, 0, 1)                 // up vector
-	light      = V(0.1, 0.3, 1).Normalize() // light direction
+	light      = V(0.5, 0.3, 1).Normalize() // light direction
 	color      = HexColor("#468966")        // object color
 	background = HexColor("#FFF8E3")        // background color
 )
@@ -38,6 +38,7 @@ func main() {
 	}
 	MeshEdges(mesh)
 	mesh.BiUnitCube()
+	mesh.Transform(Rotate(up, Radians(0)))
 	// mesh.SmoothNormals()
 	fmt.Println(len(mesh.Triangles))
 
@@ -55,10 +56,11 @@ func main() {
 
 	context.Shader = NewSolidColorShader(matrix, Black)
 	context.Wireframe = true
-	context.LineWidth = scale * 1
-	context.DepthBias = -4e-5
-	// context.DrawLines(mesh.Lines)
+	context.DepthBias = -1e-4
+	context.LineWidth = scale / 2.0
 	context.DrawTriangles(mesh.Triangles)
+	context.LineWidth = scale * 3
+	context.DrawLines(mesh.Lines)
 
 	// downsample image for antialiasing
 	image := context.Image()
--- simplify.go
+++ simplify.go
@@ -2,6 +2,7 @@ package fauxgl
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
@@ -9,24 +10,71 @@ type edge struct {
 	A, B Vector
 }
 
-func makeEdge(a, b Vector) edge {
+func sortedEdge(a, b Vector) edge {
 	if b.Less(a) {
 		a, b = b, a
 	}
 	return edge{a, b}
 }
 
+func edgesToRings(edges []edge) [][]Vector {
+	pointToEdge := make(map[Vector]edge)
+	pending := make(map[edge]bool)
+	for _, e := range edges {
+		pointToEdge[e.A] = e
+		pending[e] = true
+	}
+	var rings [][]Vector
+	for len(pending) > 0 {
+		var e edge
+		for e, _ = range pending {
+			break
+		}
+		var ring []Vector
+		for pending[e] {
+			ring = append(ring, e.A)
+			delete(pending, e)
+			e = pointToEdge[e.B]
+		}
+		ring = simplifyRing(ring)
+		rings = append(rings, ring)
+	}
+	return rings
+}
+
+func simplifyRing(ring []Vector) []Vector {
+	n := len(ring)
+	straight := make([]bool, len(ring))
+	start := -1
+	for i := 0; i < n; i++ {
+		p1 := ring[(i+n-1)%n]
+		p2 := ring[i]
+		p3 := ring[(i+1)%n]
+		v1 := p2.Sub(p1).Normalize()
+		v2 := p3.Sub(p2).Normalize()
+		c := v1.Dot(v2)
+		straight[i] = math.Abs(c-1) < 1e-9
+		if start < 0 && !straight[i] {
+			start = i
+		}
+	}
+	var result []Vector
+	for i := start; i < start+n; i++ {
+		j := i % n
+		if !straight[j] {
+			result = append(result, ring[j])
+		}
+	}
+	return result
+}
+
 func MeshEdges(mesh *Mesh) {
 	// map edges to faces
 	edgeTriangles := make(map[edge][]*Triangle)
 	for _, t := range mesh.Triangles {
-		v1 := t.V1.Position
-		v2 := t.V2.Position
-		v3 := t.V3.Position
-		fmt.Println(v1)
-		e1 := makeEdge(v1, v2)
-		e2 := makeEdge(v2, v3)
-		e3 := makeEdge(v3, v1)
+		e1 := sortedEdge(t.V1.Position, t.V2.Position)
+		e2 := sortedEdge(t.V2.Position, t.V3.Position)
+		e3 := sortedEdge(t.V3.Position, t.V1.Position)
 		edgeTriangles[e1] = append(edgeTriangles[e1], t)
 		edgeTriangles[e2] = append(edgeTriangles[e2], t)
 		edgeTriangles[e3] = append(edgeTriangles[e3], t)
@@ -43,23 +91,48 @@ func MeshEdges(mesh *Mesh) {
 		var group []*Triangle
 		group = append(group, t)
 		seen[t] = true
+		interiorEdges := make(map[edge]bool)
 		for i := 0; i < len(group); i++ {
 			t := group[i]
-			e1 := makeEdge(t.V1.Position, t.V2.Position)
-			e2 := makeEdge(t.V2.Position, t.V3.Position)
-			e3 := makeEdge(t.V3.Position, t.V1.Position)
+			e1 := sortedEdge(t.V1.Position, t.V2.Position)
+			e2 := sortedEdge(t.V2.Position, t.V3.Position)
+			e3 := sortedEdge(t.V3.Position, t.V1.Position)
 			for _, e := range []edge{e1, e2, e3} {
 				for _, u := range edgeTriangles[e] {
-					if t == u || seen[u] {
+					if t == u || !n.NearEqual(u.Normal(), 1e-9) {
 						continue
 					}
-					if !n.NearEqual(u.Normal(), 1e-9) {
-						mesh.Lines = append(mesh.Lines, NewLineForPoints(e.A, e.B))
+					interiorEdges[e] = true
+					if seen[u] {
 						continue
 					}
 					group = append(group, u)
 					seen[u] = true
 				}
+			}
+		}
+		var exteriorEdges []edge
+		for _, t := range group {
+			v1 := t.V1.Position
+			v2 := t.V2.Position
+			v3 := t.V3.Position
+			if !interiorEdges[sortedEdge(v1, v2)] {
+				exteriorEdges = append(exteriorEdges, edge{v1, v2})
+			}
+			if !interiorEdges[sortedEdge(v2, v3)] {
+				exteriorEdges = append(exteriorEdges, edge{v2, v3})
+			}
+			if !interiorEdges[sortedEdge(v3, v1)] {
+				exteriorEdges = append(exteriorEdges, edge{v3, v1})
+			}
+		}
+		rings := edgesToRings(exteriorEdges)
+		for _, ring := range rings {
+			n := len(ring)
+			for i := range ring {
+				p1 := ring[i]
+				p2 := ring[(i+1)%n]
+				mesh.Lines = append(mesh.Lines, NewLineForPoints(p1, p2))
 			}
 		}
 		groups = append(groups, group)
