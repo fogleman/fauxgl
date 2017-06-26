9d52672c870bb8a4295465c4191698bd472e3034
Michael Fogleman <fogleman@gmail.com>
2017-06-26 12:48:40 -0400

    performance improvement

--- util.go
+++ util.go
@@ -107,6 +107,8 @@ func Round(a float64) int {
 }
 
 func RoundPlaces(a float64, places int) float64 {
-	shift := math.Pow(10, float64(places))
+	shift := powersOfTen[places]
 	return float64(Round(a*shift)) / shift
 }
+
+var powersOfTen = []float64{1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16}
